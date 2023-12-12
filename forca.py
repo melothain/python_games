6236325a94d72e663fad4f66ca9da45a0cadb4f3
Thainá <melo.thain@gmail.com>
2023-12-12 14:06:20 -0300

    Adicionando finalização de loop enforcou.

--- forca.py
+++ forca.py
@@ -3,7 +3,7 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "alface"
     palavra_secreta = palavra_secreta.lower()
     
     letras_acertadas = ["_", "_" , "_" , "_" , "_" , "_"]
@@ -12,16 +12,22 @@ def jogar():
 
     enforcou = False
     acertou = False
+    erros = 0
 
     while not enforcou and not acertou: 
         chute = input('Qual letra? ')
         chute = chute.strip().lower()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute == letra):
-                letras_acertadas[index] = letra
-            index += 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
         print(letras_acertadas)
 
         print("Jogando...")
