7ee39dda782f8d94653a87876e4481868be9af2d
Thainá <melo.thain@gmail.com>
2023-12-12 14:57:28 -0300

    Adicionando leitura randomica da palavra secreta.

--- forca.py
+++ forca.py
@@ -5,8 +5,19 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "paralelepipedo"
-    palavra_secreta = palavra_secreta.lower()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+
+    palavra_secreta = palavras[numero].lower()
     
     letras_acertadas = ["_" for letra in palavra_secreta]
 
